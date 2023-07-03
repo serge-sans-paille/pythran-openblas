e12357e1a578ebb944fa705c85fab307b4fa4223
kbesrour-ma <kbesrour@meilleursagents.com>
2023-07-03 13:53:15 +0200

    feat: add mac arm biuld

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from shutil import copyfile, copytree
 import glob
 
 OpenBLASVersion = '0.3.23'
-ProjectVersion = f"{OpenBLASVersion}preview4"
+ProjectVersion = f"{OpenBLASVersion}preview5"
 name = 'python_openblas_build'
 
 
